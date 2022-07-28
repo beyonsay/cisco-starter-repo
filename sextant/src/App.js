8cc1689234d53d0337f959dce090a39e8e61c86d
Lwazi <lwazisibeko48@gmail.com>
2022-07-28 17:55:48 +0200

    Creates basic placing of banner and child cards

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
 
+
+function ChildCard() {
+  return(
+    <div className="cardContents">
+      Filler
+    </div>
+  ) 
+}
+
 function App() {
   return (
     <div className="App">
-      
+      <div className="Banner">
+        <header>
+          SEXTANT
+        </header>
+      </div>
+
+      <div className="Exhibit">
+        <header>
+          Exhibit
+        </header>
+
+        <ChildCard/>
+      </div>
     </div>
   );
 }
